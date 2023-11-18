6c14fa0cfd0469776fb80e57709c88874c55e00a
Drannien <148342386+Drannien@users.noreply.github.com>
2023-11-18 17:35:43 +0100

    Update Version3.java

--- src/main/java/com/mycompany/mbpracica1/Version3.java
+++ src/main/java/com/mycompany/mbpracica1/Version3.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.mbpracica1;
 
+import static com.mycompany.mbpracica1.Version2.lanzaConsulta;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -24,17 +25,13 @@ import org.apache.solr.common.SolrDocument;
 public class Version3 {
     
     public static int nConsultaParaCisi = 1;
+    static int nConsulta = 1;
    
     public static void main(String[] args) throws IOException,
 			SolrServerException {
        
-       int nConsulta = 1;
+       
 
-       
-       
-       
-       HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/motores5").build();
-       SolrQuery query = new SolrQuery();
        
        String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\CISI.QRY";
        File filename = new File(ruta);
@@ -42,46 +39,81 @@ public class Version3 {
        int cuenta = 0;
        StringBuilder palabras = new StringBuilder();
        
-       
-       while(sc.hasNext() && cuenta < 5)
-       {
+       while(sc.hasNext())
+        {
            
-           String pal = sc.next();
-           if(!pal.matches("[0-9].*|\\..*"))
+           
+           String linea = sc.nextLine();
+           if(linea.startsWith(".I"))
            {
-            palabras.append(pal). append(" ");
-            System.out.println(pal + " ");
-            cuenta ++;
-           }
-       }
+               palabras.setLength(0);
+               cuenta = 0;
+               
+                while(cuenta <5)
+                {
+                String pal = sc.next();
+                palabras.append(pal). append(" ");
+                cuenta++;
+                
+                if(cuenta  ==5)
+                {
+                String consulta = palabras.toString().trim();
+                String consulta1 = " ";
+                consulta1 = eliminarPrimeraPalabra(consulta);
+                lanzaConsulta(consulta1);
+                }
+               }
+            }
+        }
        
        
        
-       sc.close();
        
-       String consulta = palabras.toString().trim();
        
+       
+    }   
+    
+    
+    private static void lanzaConsulta(String consulta) throws SolrServerException, IOException   
+    {
+       HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/motores5").build();
+       SolrQuery query = new SolrQuery();
+       String palabras = consulta.replaceAll("[^a-zA-Z0-9 ]", "");
+       //System.out.println(palabras); 
        query.setQuery("*");
        query.setFields("autor", "titulo", "score", "idp");
-       query.addFilterQuery("texto: " + consulta);
+       query.addFilterQuery("texto: " + palabras);
        QueryResponse rsp = solr.query(query);
        SolrDocumentList docs = rsp.getResults();
-       
-       
-       //for (int i = 0; i<docs.size(); i++)
-       //{
-           //System.out.println(docs.get(i));
-       //}
        converTrec(docs,nConsulta);
+       //System.out.println(nConsulta);
        nConsulta++;
+       
+       
+       
     }
     
-    private static void converTrec(SolrDocumentList docs, int nConsulta) throws IOException
+    private static String eliminarPrimeraPalabra(String input) {
+        if (input == null || input.isEmpty()) {
+            return ""; // Devuelve una cadena vacía si el input es nulo o está vacío
+        }
+
+        int indiceEspacio = input.indexOf(' ');
+        if (indiceEspacio != -1) { // Si se encuentra al menos un espacio en el string
+            return input.substring(indiceEspacio + 1).trim(); // Devuelve el string después del primer espacio
+        } else {
+            return ""; // Si no se encuentra ningún espacio, devuelve una cadena vacía
+        }
+
+    }
+
+    private static void converTrec1(SolrDocumentList docs, int nConsulta) throws IOException
     {
-        int nranking = 0;
-        String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\miTrec.TREC";
+        
+        String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\miTrec1.txt";
         File filename = new File(ruta);
-        BufferedWriter escritor = new BufferedWriter(new FileWriter(filename));
+        BufferedWriter escritor = new BufferedWriter(new FileWriter(filename,true));
+        int nRanking = 1;
         for (SolrDocument doc: docs)
         {
             Object titulo = doc.getFieldValue("titulo");
@@ -89,10 +121,26 @@ public class Version3 {
             Object score  = doc.getFieldValue("score");
             Object idp = doc.getFieldValue("idp");
             
-            System.out.println("Título: " + titulo);
-            System.out.println("Autor: " + autor);
-            System.out.println("Score: " + score);
-            System.out.println("IDP: " + idp);
+            escritor.write(nConsulta + " " + "Q0" + " " + idp + " " + nRanking + " " + score + " " + "Luismi" + " " + "\n" );
+            nRanking++;
+            
+        }
+        escritor.close();
+        
+    }
+    
+    private static void converTrec(SolrDocumentList docs, int nConsulta) throws IOException
+    {
+        int nranking = 0;
+        String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\miTrec.TREC";
+        File filename = new File(ruta);
+        BufferedWriter escritor = new BufferedWriter(new FileWriter(filename,true));
+        for (SolrDocument doc: docs)
+        {
+            Object titulo = doc.getFieldValue("titulo");
+            Object autor = doc.getFieldValue("autor");
+            Object score  = doc.getFieldValue("score");
+            Object idp = doc.getFieldValue("idp");
             
             String tituloSTR = titulo.toString();
             String autorSTR = autor.toString();
@@ -117,7 +165,7 @@ public class Version3 {
         File filename = new File(ruta);
         File filename2 = new File(ruta2);
         Scanner scan = new Scanner(filename);
-        BufferedWriter escritor = new BufferedWriter(new FileWriter(filename2));
+        BufferedWriter escritor = new BufferedWriter(new FileWriter(filename2,true));
        
         while(scan.hasNextLine())
         {
@@ -134,9 +182,9 @@ public class Version3 {
             int scoreInt = (int) scoreFl;
             String scoreSTR = String.valueOf(scoreInt);
             
-            System.out.println(consulta);
-            System.out.println(documento);
-            System.out.println(scoreSTR);
+            //System.out.println(consulta);
+            //System.out.println(documento);
+            //System.out.println(scoreSTR);
            
            
             escritor.write(consulta + " " + cero + " " + documento + " " + scoreSTR + "\n");
