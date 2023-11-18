d9ae6c2f0541416daf7f8246800f9edc681032ba
Drannien <148342386+Drannien@users.noreply.github.com>
2023-11-18 17:36:16 +0100

    Update Version2.java

--- src/main/java/com/mycompany/mbpracica1/Version2.java
+++ src/main/java/com/mycompany/mbpracica1/Version2.java
@@ -3,8 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mbpracica1;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 import org.apache.solr.client.solrj.SolrClient;
@@ -24,68 +26,82 @@ public class Version2 {
     
     public static void main(String[] args) throws IOException,
 			SolrServerException {
-
+        
        
-       HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/motores4").build();
-       SolrQuery query = new SolrQuery();
-       
-       String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\CISI.QRY";
-       File filename = new File(ruta);
-       Scanner sc = new Scanner(filename);
-       int cuenta = 0;
-       StringBuilder palabras = new StringBuilder();
-       
-       
-       while(sc.hasNext() && cuenta < 5)
-       {
+        
+        
+        
+        
+        
+        
+        //StringBuilder palabras = new StringBuilder();
+        String ruta = "C:\\Users\\luism\\OneDrive\\Documentos\\NetBeansProjects\\PRACTICAMOTORES\\CISI.QRY";
+        File filename = new File(ruta);
+        Scanner sc = new Scanner(filename);
+        StringBuilder palabras = new StringBuilder();
+        int cuenta = 0;
+        
+        while(sc.hasNext())
+        {
            
-           String pal = sc.next();
-           if(!pal.matches("[0-9].*|\\..*"))
+           
+           String linea = sc.nextLine();
+           if(linea.startsWith(".I"))
            {
-            palabras.append(pal). append(" ");
-            System.out.println(pal + " ");
-            cuenta ++;
-           }
-       }
-       
-       
-       
-       sc.close();
-       
-       String consulta = palabras.toString().trim();
+               palabras.setLength(0);
+               cuenta = 0;
+               
+                while(cuenta <5)
+                {
+                String pal = sc.next();
+                palabras.append(pal). append(" ");
+                cuenta++;
+                
+                if(cuenta  ==5)
+                {
+                String consulta = palabras.toString().trim();
+                String consulta1 = " ";
+                consulta1 = eliminarPrimeraPalabra(consulta);
+                System.out.println(consulta1);
+                lanzaConsulta(consulta1);
+                }
+               }
+            }
+        }
+    }
+
+    public static void lanzaConsulta(String palabras) throws SolrServerException, IOException
+    {
+        
+       HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/motores5").build();
+       SolrQuery query = new SolrQuery();
+       String palabras1 = palabras.replaceAll("[^a-zA-Z0-9 ]", "");
+       System.out.println(palabras1);
        query.setQuery("*");
-       
-       SolrQuery query2 = new SolrQuery();
-       query2.setQuery("score");
-      
-       query.addFilterQuery("texto: " + consulta);
-       query2.addFilterQuery("texto: " + consulta);
-       
+       query.setFields("autor", "titulo", "score", "idp");
+       query.addFilterQuery("texto: " + palabras1);
        QueryResponse rsp = solr.query(query);
-       QueryResponse rsp2 = solr.query(query2);
-       
        SolrDocumentList docs = rsp.getResults();
-       SolrDocumentList docs2 = rsp2.getResults();
-       
        for (int i = 0; i<docs.size(); i++)
        {
            System.out.println(docs.get(i));
        }
-       
-       
-       //Pasar los rankings a converTrec y en converTrec crear el Trec File.
-       //converTrec(Rankings)
-       //
-       
-
-        
-        
-        
-        
+    
     }
     
-    private static void converTrec()
-    {
-        
+    private static  String eliminarPrimeraPalabra(String input) {
+    if (input == null || input.isEmpty()) {
+        return ""; // Devuelve una cadena vacía si el input es nulo o está vacío
     }
+
+    int indiceEspacio = input.indexOf(' ');
+    if (indiceEspacio != -1) { // Si se encuentra al menos un espacio en el string
+        return input.substring(indiceEspacio + 1).trim(); // Devuelve el string después del primer espacio
+    } else {
+        return ""; // Si no se encuentra ningún espacio, devuelve una cadena vacía
+    }
+}
+
+
+
 }
